563e4de4ffb62fdeb8fbd64642738f0e4607d4e6
salfedev <sal.abuewilly@capgemini.com>
2023-07-15 01:50:01 +0100

    Add firebase service

---
+++ src/firebase.js
@@ -0,0 +1,52 @@
+import { log } from "./logger.js";
+import { initializeApp } from "firebase/app";
+// firebase database
+import { getDatabase, ref, set, onValue } from "firebase/database";
+// firebase auth
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+// Initialize Firebase
+log('Intialiazing with: ', firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// write note to firebase
+const writeNoteData = async (userId, note) => {
+  set(ref(db, `users/${userId}/notes/${note.id}`), note);
+};
+
+// read all user's notes from firebase
+const readNotesData = (userId, callback) => {
+  const notesRef = ref(db, `users/${userId}/notes`);
+  onValue(notesRef, (snapshot) => {
+    const data = snapshot.val();
+    callback(data);
+  });
+};
+
+// read single note from firebase
+const readNoteData = (userId, noteId, callback) => {
+  const noteRef = ref(db, `users/${userId}/notes/${noteId}`);
+  onValue(noteRef, (snapshot) => {
+    const data = snapshot.val();
+    callback(data);
+  });
+};
+
+
+module.exports = {
+  writeNoteData,
+  readNotesData,
+  readNoteData,
+};
