a11885db89019a843fe84bba8cf2d992ba76a1ea
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-07-15 03:52:35 +0100

    Feat/get note (#3)
    * clean snyk errors
    
    * Add readme
    
    * coverage
    
    * update circle ci conf
    
    * read all notes and read a single note

---
+++ README.md
@@ -0,0 +1,5 @@
+ <!-- CircleCI Badge: -->
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/salfedev/express_firebase_crud/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/salfedev/express_firebase_crud/tree/master)
+# REST API CRUD with Firebase
+## Notes:
+- [ ] --
--- src/index.js
+++ src/index.js
@@ -2,44 +2,51 @@ import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 import { log, log_green, log_red, log_yellow } from "./logger.js";
-import { writeNoteData } from "./firebase.js";
-
+import { writeNoteData, readNotesData, readNoteData } from "./firebase.js";
+import uuid4 from "uuid4";
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
-
-app.get("/api/notes", (req, res) => {
-  log("GET HTTP method on /notes resource");
-  return res.send("GET HTTP method on /notes resource");
+app.get("/api/notes", async (req, res) => {
+  log_green("GET HTTP method on /notes resource");
+  // return res.send("GET HTTP method on /notes resource");
+  const userId = req.query?.userId || null;
+  const noteId = req.query?.noteId || null;
+  if (noteId) {
+    await readNoteData(userId, noteId, (data) => {
+      return res.send(data);
+    });
+  }
+  if (!userId) {
+    return res.send("No user id provided");
+  }
+  await readNotesData(userId, (data) => {
+    return res.send(data);
+  });
 });
-
 app.post("/api/notes", async (req, res) => {
+  const { userId, title, content } = req.query;
+  log_yellow(userId, title, content);
   const note = {
-    userId: "1",
-    id: "1",
-    title: "My first note",
-    content: "Hello world!",
+    userId: userId,
+    id: uuid4(),
+    title: title,
+    content: content,
   };
   await writeNoteData(note.userId, note).then((response) => {
     log_yellow("Note written to the database");
     return res.send(response);
   })
   .catch((error) => {
-    log_red("Note failed to write to the database", error);
+    log_red("Failed to write note to the database", error);
   });
 });
-
 app.put("/api/notes/:noteID", (req, res) => {
   return res.send(`PUT HTTP method on /notes/${req.params.noteID} resource`);
 });
-
 app.delete("/api/notes/:noteID", (req, res) => {
   return res.send(`DELETE HTTP method on /notes/${req.params.noteID} resource`);
 });
-
 app.listen(port, () => {
   log_green(`Server listening on port ${port}!`);
 });
-
-
-
