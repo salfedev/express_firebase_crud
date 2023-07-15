09b2068bd673abe43d1a171f01a9d215897dd6a7
salfedev <sal.abuewilly@capgemini.com>
2023-07-15 04:43:29 +0100

    delete notes

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import {
   readNotes,
   readNote,
   updateNote,
+  deleteNote,
 } from "./firebase.js";
 import uuid4 from "uuid4";
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
+// get all notes, or a single note
 app.get("/api/notes", async (req, res) => {
   log_green("GET HTTP method on /notes resource");
   const userId = req.query?.userId || null;
@@ -29,6 +31,7 @@ app.get("/api/notes", async (req, res) => {
     return res.send({ error: "No user ID provided"});
   }
 });
+// create note
 app.post("/api/notes", async (req, res) => {
   const { userId, title, content, tags } = req.query;
   log_yellow(userId, title, content);
@@ -48,6 +51,7 @@ app.post("/api/notes", async (req, res) => {
     return error;
   });
 });
+// update note
 app.put("/api/notes/", async (req, res) => {
   const { userId, noteId, title, content, tags } = req.query;
   log_yellow(`PUT HTTP method on /notes/${noteId} resource`);
@@ -67,10 +71,20 @@ app.put("/api/notes/", async (req, res) => {
     return error;
   });
 });
-app.delete("/api/notes/:noteID", (req, res) => {
+// delete note
+app.delete("/api/notes/", async (req, res) => {
   log_red(`DELETE HTTP method on /notes/${req.params.noteID} resource`);
   const { userId, noteId } = req.query;
+  await deleteNote(userId, noteId).then((response) => {
+    log_yellow("Note deleted from the database");
+    return res.send(response);
+  })
+  .catch((error) => {
+    log_red("Failed to delete note from the database", error);
+    return error;
+  });
 });
+// start the Express server
 app.listen(port, () => {
   log_green(`Server listening on port ${port}!`);
 });
