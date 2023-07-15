4a5c13c03c14673573d156b5b1c1e5d8a88bef0a
salfedev <sal.abuewilly@capgemini.com>
2023-07-15 04:18:47 +0100

    Update notes, add tags

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 import { log, log_green, log_red, log_yellow } from "./logger.js";
-import { writeNoteData, readNotesData, readNoteData } from "./firebase.js";
+import {
+  writeNote,
+  readNotes,
+  readNote,
+  updateNote,
+} from "./firebase.js";
 import uuid4 from "uuid4";
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
 app.get("/api/notes", async (req, res) => {
   log_green("GET HTTP method on /notes resource");
-  // return res.send("GET HTTP method on /notes resource");
   const userId = req.query?.userId || null;
   const noteId = req.query?.noteId || null;
   if (noteId) {
-    await readNoteData(userId, noteId, (data) => {
+    await readNote(userId, noteId, (data) => {
       return res.send(data);
     });
   }else {
-    await readNotesData(userId, (data) => {
+    await readNotes(userId, (data) => {
       return res.send(data);
     });
   }
@@ -26,15 +30,16 @@ app.get("/api/notes", async (req, res) => {
   }
 });
 app.post("/api/notes", async (req, res) => {
-  const { userId, title, content } = req.query;
+  const { userId, title, content, tags } = req.query;
   log_yellow(userId, title, content);
   const note = {
     userId: userId,
     id: uuid4(),
     title: title,
     content: content,
+    tags: tags ? tags.split(",") : []
   };
-  await writeNoteData(note.userId, note).then((response) => {
+  await writeNote(note.userId, note).then((response) => {
     log_yellow("Note written to the database");
     return res.send(response);
   })
@@ -42,8 +47,20 @@ app.post("/api/notes", async (req, res) => {
     log_red("Failed to write note to the database", error);
   });
 });
-app.put("/api/notes/:noteID", (req, res) => {
-  return res.send(`PUT HTTP method on /notes/${req.params.noteID} resource`);
+app.put("/api/notes/:noteID", async (req, res) => {
+  log_yellow(`PUT HTTP method on /notes/${req.params.noteID} resource`);
+  const { userId, noteId, title, content, tags } = req.query;
+  const note = {
+    userId,
+    title,
+    content,
+    id: noteId,
+    tags: tags ? tags.split(",") : []
+  };
+  await updateNote(userId, noteId, note).then((response) => {
+    log_yellow("Note updated in the database");
+    return res.send(response);
+  });
 });
 app.delete("/api/notes/:noteID", (req, res) => {
   return res.send(`DELETE HTTP method on /notes/${req.params.noteID} resource`);
