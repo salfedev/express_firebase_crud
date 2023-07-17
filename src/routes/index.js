b3b5fcb3188cc306e4e9da4e27d35a97b85a807c
salfedev <sal.abuewilly@capgemini.com>
2023-07-17 02:43:43 +0100

    clean up

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-import notes from "./notes.js";
-import teams from "./teams.js";
-import boards from "./boards.js";
+import notes from "./api/notes.js";
+import teams from "./api/teams.js";
+import boards from "./api/boards.js";
 module.exports = (app, db, logger) => {
   notes(app, db, logger);
   teams(app, db, logger);
